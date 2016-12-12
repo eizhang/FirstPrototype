43707c54a4ab931be1a18b9d053be21afc0a0d35
Anthony Liu <liu916@gmail.com>
2016-12-11 22:19:12 -0500

    context 2d

--- text_block.js
+++ text_block.js
@@ -2,9 +2,18 @@ class TextBlock {
     constructor(text, cv) {
         this.text = text;
         this.cref = cv;
+        this.highlight = false;
     }
     draw(w, h) {
-        this.cref.font = "30px arial";
-        this.cref.fillText(this.text, w, h);
+        var ctxt = this.cref.getContext("2d");
+        if (this.highlight)
+        {
+            ctxt.fillStyle = "blue";
+        }
+        ctxt.font = "30px arial";
+        ctxt.fillText(this.text, w, h);
     }
+}
+
+class Sentence {
 }
